b433a1069e5ccf8f97761a433eede9c703c3ea52
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2023-12-01 23:57:53 +0100

    :sparkles: (split data) add script to split the dataset

---
+++ backend/src/data/split_data.py
@@ -0,0 +1,30 @@
+import argparse
+import pandas as pd
+from load_data import read_params
+from sklearn.model_selection import train_test_split
+
+
+def split_data(df, train_data_path, test_data_path, split_ratio, random_state):
+    train, test = train_test_split(
+        df, test_size=split_ratio, random_state=random_state)
+    train.to_csv(train_data_path, sep=',', index=False, encoding='utf-8')
+    test.to_csv(test_data_path, sep=',', index=False, encoding='utf-8')
+
+
+def split_and_saved_data(config_path):
+    config = read_params(config_path)
+    raw_data_path = config['raw_data_config']['raw_data_csv']
+    test_data_path = config['processed_data_config']['test_data_csv']
+    train_data_path = config['processed_data_config']['train_data_csv']
+    split_ratio = config['raw_data_config']['train_test_split_ratio']
+    random_state = config['raw_data_config']['random_state']
+    raw_df = pd.read_csv(raw_data_path)
+    split_data(raw_df, train_data_path, test_data_path,
+               split_ratio, random_state)
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('--config', default='params.yml')
+    parsed_args = args.parse_args()
+    split_and_saved_data(config_path=parsed_args.config)
