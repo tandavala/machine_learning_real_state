838a3aa7a56f78ac1fa9cbaf05356a47a47b8677
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2023-12-01 22:45:42 +0100

    :sparkles: (project boilerplate) iniciate the project

---
+++ backend/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a project that is part of the series of articles that I am developing with the theme Uncomplicating Machine Learning. The objective of this project is to demonstrate, in practice, how to develop a machine learning model, following best practices and using standard tools in the software industry. With this initiative, I intend to consolidate and document my learning process in Machine Learning, providing a practical and accessible vision for readers interested in the topic.',
+    author='José Tandavala',
+    license='MIT',
+)
